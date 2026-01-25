854d7928cbe985a62be82b63c2fd2910541d2a13
semomhamd <semomohamed010@gmail.com>
2026-01-25 13:42:19 -0800

    Update cleaner_pro.py

--- cleaner_pro.py
+++ cleaner_pro.py
@@ -1,4 +1,5 @@
-​import streamlit as st
-​def run_module():
-st.info("🛠️ This tool is under calibration by MIA8444.")
-st.write("The Beast is gathering resources...")
+import streamlit as st
+
+def run_module():
+    st.info("🛠️ This tool is under calibration by MIA8444.")
+    st.write("The Beast is preparing the algorithms...")
