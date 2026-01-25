3fa82e3f3df3e185f749901605cd1cdb659e980a
semomhamd <semomohamed010@gmail.com>
2026-01-25 13:09:20 -0800

    Update sql_beast.py

--- sql_beast.py
+++ sql_beast.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 def run_module():
-    st.markdown("### 🛠️ This tool is under construction by MIA8444")
-    st.info("The Beast is gathering resources for this module...")
+    st.info("🛠️ This module is currently being calibrated by MIA8444.")
+    st.markdown("---")
+    st.write("The Beast is gathering the necessary algorithms to activate this tool.")
