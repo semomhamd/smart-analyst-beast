de4bf3f177426c62c7667c6beece32fb8c79cf83
semomhamd <semomohamed010@gmail.com>
2026-01-26 10:31:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+# © 2026 MIA8444 | THE BEAST v3.0
 import streamlit as st
 from auth_system import login_page
 
-# ضبط إعدادات الصفحة الأساسية
+# 1. إعدادات الصفحة الأساسية
 st.set_page_config(page_title="Smart Analyst Beast v3.0", layout="wide")
 
-# تشغيل واجهة الدخول الذهبية
+# 2. استدعاء واجهة الدخول الذهبية الخاصة بـ MIA8444
 login_page()
