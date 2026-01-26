c4dbb43583edeb8f3176ca651cc8385f8e9889e8
semomhamd <semomohamed010@gmail.com>
2026-01-26 10:10:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 from auth_system import login_page
 
-# إعدادات الصفحة
+# ضبط إعدادات الصفحة
 st.set_page_config(page_title="Smart Analyst Beast v3.0", layout="wide")
 
-# تشغيل واجهة الدخول
+# تشغيل واجهة الدخول الذهبية
 login_page()
